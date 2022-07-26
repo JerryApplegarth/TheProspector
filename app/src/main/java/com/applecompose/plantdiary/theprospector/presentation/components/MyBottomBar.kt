26a2cba0c979e2e7cf3c1083a927357b6ef10253
Jerry applegarth <applegarth1630@gmail.com>
2022-07-25 17:54:04 -0700

    basic packages

---
+++ app/src/main/java/com/applecompose/plantdiary/theprospector/presentation/components/MyBottomBar.kt
@@ -0,0 +1,8 @@
+package com.applecompose.plantdiary.theprospector.presentation.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyBottomBar() {
+
+}
---
+++ app/src/main/java/com/applecompose/plantdiary/theprospector/presentation/components/MyTopBar.kt
@@ -0,0 +1,4 @@
+package com.applecompose.plantdiary.theprospector.presentation.components
+
+class MyTopBar {
+}
